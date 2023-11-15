b109d9cefa98e6acf3b6f785924a6fd85255d9f8
unknown <chrishancode@gmail.com>
2023-11-15 14:15:21 -0800

    adding redis to docker compose file and main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import redis
 
 app = FastAPI()
+
+r = redis.Redis(host="redis", port=6379)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World!!"}
+
+@app.get("/hits")
+def read_root():
+    r.incr("hits")
+    return {"number of hits": r.get("hits")}
